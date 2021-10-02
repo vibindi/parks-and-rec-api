e0c96e89f76bfe89e59be18ef2919ec2abd2d983
vibindi <v.bindiganavile@gmail.com>
2021-10-02 04:23:55 -0400

    testing home page

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import pandas as pd
 import random
+import requests
+
 app = Flask(__name__)
 
 @app.route('/a/', methods=['GET'])
@@ -19,15 +21,19 @@ def randomQuote():
     data = data.to_dict()
     speaker = data.get('speaker')
     quote = data.get('quote')
-    response["Speaker"] = speaker
-    response["Quote"] = quote
+    index = random.randint(0,len(speaker) - 1)
+    response["Speaker"] = speaker.get(index)
+    response["Quote"] = quote.get(index)
     response["Code"] = 200
     return jsonify(response)
 
-# A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    rand_quote = requests.get('https://parks-and-rec-quotes.herokuapp.com/r').json()
+    print(type(rand_quote))
+    q = rand_quote.get("Quote")
+    s = rand_quote.get("Speaker")
+    return render_template('index.html', quote=q, speaker=s)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
