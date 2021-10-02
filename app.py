8c79bab0441b0cb70e7a6a826e5768beaab06782
vibindi <v.bindiganavile@gmail.com>
2021-10-02 03:24:25 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
