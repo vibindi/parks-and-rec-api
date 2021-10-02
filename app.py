66c167a4cbee7f84545ee64e56ba3d7d3dab71dd
vibindi <v.bindiganavile@gmail.com>
2021-10-02 03:39:04 -0400

    testing out simple api implementation

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
-from flask import Flask, render_template, redirect
-from flask_restful import Resource, Api, reqparse
+from flask import Flask, render_template, redirect, request, jsonify
 import pandas as pd
 import random
 import requests
 import ast
 
 app = Flask(__name__)
-api = Api(app)
-
-headers = {
-    'content-type': "application/json",
-    'cache-control': "no-cache"
-}
 
 @app.route('/')
 def index():
@@ -20,9 +13,26 @@ def index():
     #s = rand_quote.get("quote").get("speaker")
     return render_template('index.html', quote="q", speaker="s")
 
+
+@app.route('/r/', methods=['GET'])
+def randomQuote():
+    response = {}
+
+    response["MESSAGE"] = "RANDOM"
+
+    return jsonify
+
+@app.route('/a/', methods=['GET'])
+def allQuotes():
+    response = {}
+
+    response["MESSAGE"] = "ALL"
+
+    return jsonify
+
 # API Endpoints
 
-class AllQuotes(Resource):
+'''class AllQuotes(Resource):
     def get(self):
         data = pd.read_csv('data/quotes.csv')
         data = data.to_dict()
@@ -38,7 +48,7 @@ class RandomQuote(Resource):
         return {'quote': {'speaker': speakers.get(index), 'quote': quotes.get(index)}}, 200
 
 api.add_resource(RandomQuote, '/r') # Get one random quote
-api.add_resource(AllQuotes, '/a') # Get all quotes
+api.add_resource(AllQuotes, '/a') # Get all quotes '''
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
