aa5e18b30c2020fc3625b52fbd730e49a9f03d31
vibindi <v.bindiganavile@gmail.com>
2021-10-02 03:41:05 -0400

    trying out sample code

--- app.py
+++ app.py
@@ -1,54 +1,50 @@
-from flask import Flask, render_template, redirect, request, jsonify
-import pandas as pd
-import random
-import requests
-import ast
-
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
+
+    # For debugging
+    print(f"got name {name}")
+
+    response = {}
+
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+
+    # Return the response in json format
+    return jsonify(response)
+
+@app.route('/post/', methods=['POST'])
+def post_something():
+    param = request.form.get('name')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome platform!!",
+            # Add this option to distinct the POST request
+            "METHOD" : "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name."
+        })
+
+# A welcome message to test our server
 @app.route('/')
 def index():
-    #rand_quote = requests.request("GET", 'https://parks-and-rec-quotes-api.herokuapp.com/r', headers=headers).json()
-    #q = rand_quote.get("quote").get("quote")
-    #s = rand_quote.get("quote").get("speaker")
-    return render_template('index.html', quote="q", speaker="s")
-
-
-@app.route('/r/', methods=['GET'])
-def randomQuote():
-    response = {}
-
-    response["MESSAGE"] = "RANDOM"
-
-    return jsonify
-
-@app.route('/a/', methods=['GET'])
-def allQuotes():
-    response = {}
-
-    response["MESSAGE"] = "ALL"
-
-    return jsonify
-
-# API Endpoints
-
-'''class AllQuotes(Resource):
-    def get(self):
-        data = pd.read_csv('data/quotes.csv')
-        data = data.to_dict()
-        return {'data': data}, 200
-
-class RandomQuote(Resource):
-    def get(self):
-        data = pd.read_csv('data/quotes.csv')
-        data = data.to_dict()
-        speakers = data.get('speaker')
-        quotes = data.get('quote')
-        index = random.randint(0,len(speakers) - 1)
-        return {'quote': {'speaker': speakers.get(index), 'quote': quotes.get(index)}}, 200
-
-api.add_resource(RandomQuote, '/r') # Get one random quote
-api.add_resource(AllQuotes, '/a') # Get all quotes '''
+    return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
